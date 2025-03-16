9dcfa29953a37cfad515fd7b56c634883f29b307
peppecaru00 <carusothe99@gmail.com>
2025-03-16 01:55:28 +0100

    SONO STANCO

--- modules/uiEffects.js
+++ modules/uiEffects.js
@@ -30,12 +30,13 @@ function openImageOverlay(imageElement) {
     document.documentElement.style.setProperty('--scroll-position', `-${scrollY}px`);
     document.body.classList.add('overlay-open');
 
+    //add zoom button
     const zoomButton = document.createElement('button');
     zoomButton.classList.add('zoom-button');
     zoomButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="11" cy="11" r="8"></circle><line x1="21" y1="21" x2="16.65" y2="16.65"></line><line x1="11" y1="8" x2="11" y2="14"></line><line x1="8" y1="11" x2="14" y2="11"></line></svg>';
     zoomButton.setAttribute('aria-label', 'Zoom image');
 
-        // Add zoom button functionality
+    // Add zoom button functionality
     let isZoomed = false;
     zoomButton.addEventListener('click', (e) => {
         e.stopPropagation();
