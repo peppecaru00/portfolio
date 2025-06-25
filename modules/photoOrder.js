cd8703f6435bab8bdde8bf1e77777dbb905e686b
Giuseppe Caruso <s334105@studenti.polito.it>
2025-06-25 12:22:12 +0200

    aggiunto missitalia

--- modules/photoOrder.js
+++ modules/photoOrder.js
@@ -9,10 +9,10 @@ export function getPhotosWithOrder() {
         { name: 'Scopello', order: 7 },
         { name: 'Venaria', order: 3 },
         { name: 'Perugia', order: 4 },
-        { name: 'Ispica', order: 5 },
+        { name: 'Ispica', order: 16 },
         { name: 'Rifugio', order: 6 },
         { name: 'Musei Reali', order: 2 },
-        { name: 'Squadra Corse PoliTO', order: 1 },
+        { name: 'Squadra Corse PoliTO', order: 5 },
         { name: 'Pool', order: 9 },
         { name: 'Sport Days', order: 10 },
         { name: 'Khamilla', order: 11 },
@@ -20,5 +20,6 @@ export function getPhotosWithOrder() {
         { name: 'Fishing', order: 13 },
         { name: 'Underpass', order: 14 },
         { name: 'Random', order: 15 },
+        { name: 'MissItalia', order: 1}
     ];
 }
