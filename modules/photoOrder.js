20df787a066b95b744917aa6ad238f22388f13b9
peppecaru00 <carusothe99@gmail.com>
2025-10-02 19:07:38 +0200

    added saloneauto, burrasca, climbing

--- modules/photoOrder.js
+++ modules/photoOrder.js
@@ -5,21 +5,24 @@
 
 export function getPhotosWithOrder() {
     return [
-        { name: 'Dorgali', order: 8 },
-        { name: 'Scopello', order: 7 },
-        { name: 'Venaria', order: 3 },
-        { name: 'Perugia', order: 4 },
-        { name: 'Ispica', order: 16 },
-        { name: 'Rifugio', order: 6 },
-        { name: 'Musei Reali', order: 2 },
-        { name: 'Squadra Corse PoliTO', order: 5 },
-        { name: 'Pool', order: 9 },
-        { name: 'Sport Days', order: 10 },
-        { name: 'Khamilla', order: 11 },
-        { name: 'Fair', order: 12 },
-        { name: 'Fishing', order: 13 },
-        { name: 'Underpass', order: 14 },
-        { name: 'Random', order: 15 },
-        { name: 'MissItalia', order: 1}
+        { name: 'Salone Auto', order: 1 },
+        { name: 'MissItalia', order: 2 },
+        { name: 'Musei Reali', order: 3 },
+        { name: 'Venaria', order: 4 },
+        { name: 'Burrasca', order: 5 },
+        { name: 'Perugia', order: 6 },
+        { name: 'Squadra Corse PoliTO', order: 7 },
+        { name: 'Rifugio', order: 8 },
+        { name: 'Scopello', order: 9 },
+        { name: 'Dorgali', order: 10 },
+        { name: 'Pool', order: 11 },
+        { name: 'Sport Days', order: 12 },
+        { name: 'Khamilla', order: 13 },
+        { name: 'Fair', order: 14 },
+        { name: 'Fishing', order: 15 },
+        { name: 'Underpass', order: 16 },
+        { name: 'Random', order: 19 },
+        { name: 'Ispica', order: 18 },
+        { name: 'Pinerolo Climbing', order: 17 }
     ];
 }
